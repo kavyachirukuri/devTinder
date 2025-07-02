de5e1c37c7ca92ea426c78ba2eb3e78baf25d7c9
kavyachirukuri <kavyachirukuri@gmail.com>
2025-07-02 17:27:33 +0530

    error handling wild card

--- src/app.js
+++ src/app.js
@@ -2,29 +2,18 @@ const express = require('express')
 
 const app = express();
 
-const { adminAuth , userAuth} = require("./middlewares/auth")
-//GET /users ==> middleware chain ==> request handler
 
-app.use('/admin', adminAuth)
-
-app.get('/admin',(req,res,next)=>{
-    console.log('Handling /user route');
-    next();
-}, 
-(req,res,next)=>{
-    res.send("1st Route Handler")
-},
-(req,res,next) => {
-    res.send('2nd Route Handler')
-}
-)
-
-app.get('/user', userAuth, (req,res,next) => {
+app.get('/getUserData', (req,res) => {
+    // logic of DB call and get user data
+    throw new Error("error found")
     res.send("User Data Sent");
 })
 
-app.post('/user/login',(req,res)=>{
-    res.send('user logged in successfully')
+app.use("/",(err,req,res,next)=>{
+    if (err) {
+        // Log your error message
+        res.status(500).send("something went wrong!!")
+    }
 })
 
 app.listen(7777, ()=>{
