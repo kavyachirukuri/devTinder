aa242b397f71c97dbeb380642212c55c477467cb
kavyachirukuri <kavyachirukuri@gmail.com>
2025-07-02 17:32:02 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -2,19 +2,24 @@ const express = require('express')
 
 const app = express();
 
-
-app.get('/getUserData', (req,res) => {
-    // logic of DB call and get user data
-    throw new Error("error found")
-    res.send("User Data Sent");
-})
-
 app.use("/",(err,req,res,next)=>{
     if (err) {
         // Log your error message
         res.status(500).send("something went wrong!!")
     }
 })
+
+app.get('/getUserData', (req,res) => {
+    // try{
+        // logic of DB call and get user data
+
+        throw new Error("error found")
+        res.send("User Data Sent");
+    // }catch (err){
+    //     res.status(500).send("Some Error contact support team")
+    // }
+})
+
 
 app.listen(7777, ()=>{
     console.log('Server is successfully listening on port 7777...')
