885274804c83528f6332e87be0161955925eab83
kavyachirukuri <kavyachirukuri@gmail.com>
2025-07-02 16:58:47 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -2,15 +2,30 @@ const express = require('express')
 
 const app = express();
 
-app.get("/user",(req,res,next)=>{
-    console.log('Handling the route user!!')
-    next()
+const { adminAuth , userAuth} = require("./middlewares/auth")
+//GET /users ==> middleware chain ==> request handler
+
+app.use('/admin', adminAuth)
+
+app.get('/admin',(req,res,next)=>{
+    console.log('Handling /user route');
+    next();
+}, 
+(req,res,next)=>{
+    res.send("1st Route Handler")
 },
-    (req,res,next)=>{
-        console.log('Handling the route user 2')
-        res.send('2nd response')
-    }
+(req,res,next) => {
+    res.send('2nd Route Handler')
+}
 )
+
+app.get('/user', userAuth, (req,res,next) => {
+    res.send("User Data Sent");
+})
+
+app.post('/user/login',(req,res)=>{
+    res.send('user logged in successfully')
+})
 
 app.listen(7777, ()=>{
     console.log('Server is successfully listening on port 7777...')
